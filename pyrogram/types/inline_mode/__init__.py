bcdf69cc142feaedf0ff3af9ca36a4ff60c2cb13
C͡linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2021-10-17 22:58:38 +0530

    😅

--- pyrogram/types/inline_mode/__init__.py
+++ pyrogram/types/inline_mode/__init__.py
@@ -29,5 +29,5 @@ from .inline_query_result_cached_document import InlineQueryResultCachedDocument
 
 __all__ = [
     "InlineQuery", "InlineQueryResult", "InlineQueryResultArticle", "InlineQueryResultPhoto",
-    "InlineQueryResultAnimation", "InlineQueryResultAudio", "InlineQueryResultVideo", "ChosenInlineResult"
+    "InlineQueryResultAnimation", "InlineQueryResultAudio", "InlineQueryResultVideo", "ChosenInlineResult",
      "InlineQueryResultCachedPhoto", "InlineQueryResultCachedDocument"]
